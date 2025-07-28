8f455dea09ac4b7868ce0518c40977f393e304ac
Diya Kithani <diyakithani.2003@gmail.com>
2025-07-27 23:38:40 -0400

    Fix score tracking and update HTML structure for Stone Paper Scissors game

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
-let userscore = 0;
-let computerscore = 0;
+let userscore = document.querySelector("#user-score-val");
+let computerscore = document.querySelector("#computer-score-val");
 let choices = document.querySelectorAll(".choice");
 let userChoice;
 let computerChoice;
-function getUserChoice() {
-  for (let choice of choices) {
-    choice.addEventListener("click", () => {
-      userChoice = choice.id;
-      console.log("User choice: " + userChoice);
-      computerChoice = getComputerChoice();
-      console.log("Computer choice: " + computerChoice);
-    });
 
-    choices.addEventListener("click", () => {
-      getUserChoice();
-    });
-  }
+for (let choice of choices) {
+  choice.addEventListener("click", () => {
+    userChoice = choice.id;
+    console.log("User choice: " + userChoice);
+    computerChoice = getComputerChoice();
+    console.log("Computer choice: " + computerChoice);
+    getResult(userChoice, computerChoice);
+  });
 }
+
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
   return choices[randomNumber].id;
 }
-getUserChoice();
+function getResult(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    console.log("It's a draw!");
+  } else if (
+    (userChoice === "rock" && computerChoice === "scissors") ||
+    (userChoice === "paper" && computerChoice === "rock") ||
+    (userChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log("You win!");
+    userscore.innerText = (parseInt(userscore.innerText, 10) || 0) + 1;
+  } else {
+    console.log("You lose!");
+    computerscore.innerText = (parseInt(computerscore.innerText, 10) || 0) + 1;
+  }
+}
